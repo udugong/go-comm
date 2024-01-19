51914f4f6561300c297f1df095afb257cedf85db
joil <joil-z@outlook.com>
2024-01-19 10:14:03 +0800

    feat: 拆分直接重试与间隔重试

--- retryable/service.go
+++ retryable/service.go
@@ -3,7 +3,6 @@ package retryable
 import (
 	"context"
 	"fmt"
-	"time"
 
 	"github.com/udugong/go-comm"
 )
@@ -13,22 +12,17 @@ type Service[T any] struct {
 	svc comm.Sender[T]
 	// 最大重试次数
 	retryMax int
-	// 重试间隔
-	interval time.Duration
 }
 
-func NewService[T any](svc comm.Sender[T], retryMax int, interval time.Duration) *Service[T] {
+func NewService[T any](svc comm.Sender[T], retryMax int) *Service[T] {
 	return &Service[T]{
 		svc:      svc,
 		retryMax: retryMax,
-		interval: interval,
 	}
 }
 
 func (s *Service[T]) Send(ctx context.Context, tpl string, args T, to ...string) error {
 	var err error
-	timer := time.NewTimer(s.interval)
-	defer timer.Stop()
 	for i := 0; i < s.retryMax; {
 		err = s.svc.Send(ctx, tpl, args, to...)
 		if err == nil {
@@ -41,8 +35,7 @@ func (s *Service[T]) Send(ctx context.Context, tpl string, args T, to ...string)
 		select {
 		case <-ctx.Done():
 			return ctx.Err()
-		case <-timer.C:
-			timer.Reset(s.interval)
+		default:
 		}
 	}
 	return fmt.Errorf("重试 %d 次都失败了; err: %w", s.retryMax, err)
